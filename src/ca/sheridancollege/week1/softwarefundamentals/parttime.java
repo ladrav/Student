0a9a3d42d2554ff4f12627544d0daa8af64ce515
test <>
2019-05-24 14:08:06 -0400

    3rd commit

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/parttime.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author ravin
+ */
+public class parttime {
+    
+        private int numhours;
+
+    /**
+     * Get the value of numhours
+     *
+     * @return the value of numhours
+     */
+    public int getNumhours() {
+        return numhours;
+    }
+
+    /**
+     * Set the value of numhours
+     *
+     * @param numhours new value of numhours
+     */
+    public void setNumhours(int numhours) {
+        this.numhours = numhours;
+    }
+
+    
+}
